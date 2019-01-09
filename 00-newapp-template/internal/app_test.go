21dad1d1fbc84cfc26bad277b5facf7ba2891c0d
KPH <whereiskurt@gmail.com>
2019-01-09 08:38:05 -0500

    -Create test config file in ./config/test/ -Fixed app_test to use os.Args

--- 00-newapp-template/internal/app_test.go
+++ 00-newapp-template/internal/app_test.go
@@ -4,22 +4,59 @@ import (
 	"00-newapp-template/internal"
 	"00-newapp-template/pkg/config"
 	"00-newapp-template/pkg/metrics"
+	"os"
 	"testing"
 )
+
+var m = metrics.NewMetrics()
 
 func TestGopherCLI(t *testing.T) {
 	t.Logf("Testing creation of CLI Application....")
 
+	os.Args = []string{"gopherit","client","list", "--mode=json"}
 	c := config.NewConfig()
-	m := metrics.NewMetrics()
+
 	SetupConfig(c)
-	internal.NewApp(c, m)
+	app := internal.NewApp(c, m)
+	app.InvokeCLI()
+}
+
+func TestGopherDefaultIsClient(t *testing.T) {
+	t.Logf("Testing creation of CLI Application....")
+
+	os.Args = []string{"gopherit","list", "--mode=json"}
+	c := config.NewConfig()
+
+	SetupConfig(c)
+	app := internal.NewApp(c, m)
+	app.InvokeCLI()
+}
+
+func TestGopherCLIHelp(t *testing.T) {
+	t.Logf("Testing creation of CLI Application....")
+
+	os.Args = []string{"gopherit","--help"}
+	c := config.NewConfig()
+	SetupConfig(c)
+	app := internal.NewApp(c, m)
+	app.InvokeCLI()
+}
+
+func TestGopherCLIClientHelp(t *testing.T) {
+	t.Logf("Testing creation of CLI Application....")
+
+	os.Args = []string{"gopherit","client", "--help"}
+	c := config.NewConfig()
+	SetupConfig(c)
+	app := internal.NewApp(c, m)
+	app.InvokeCLI()
 }
 
 func SetupConfig(c *config.Config) {
 	// Test cases are run from the package folder containing the source file.
 	c.TemplateFolder = "./../config/template/"
 	c.Client.CacheFolder = "./.." + config.DefaultClientCacheFolder
+	c.ConfigFolder = "./../config/test/"
 	c.VerboseLevel5 = true
 	c.VerboseLevel = "5"
 	c.ValidateOrFatal()
